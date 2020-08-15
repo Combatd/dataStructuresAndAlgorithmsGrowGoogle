b76cc06988b0d3281ff7d20fea35cde1bedccd75
Combatd <combatdstar@gmail.com>
2020-08-14 22:42:10 -0700

    Define LinkeList#get_position

--- list_based_collections/linkedlistpractice.py
+++ list_based_collections/linkedlistpractice.py
@@ -30,6 +30,16 @@ class LinkedList(object):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
+        counter = 1 # assume first position is 1
+        current = self.head
+        if position < 1:
+            return None # 0 and negative are not on the list
+        while current and counter <= position: # continue counter until position is found
+            if counter == position: # if the position is equal to the counter
+                return current # return the head (top node) in the list
+            current = current.next
+            counter += 1
+
         return None
     
     def insert(self, new_element, position):
@@ -58,20 +68,20 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-print ll.head.next.next.value
+print(ll.head.next.next.value)
 # Should also print 3
-print ll.get_position(3).value
+print(ll.get_position(3).value)
 
 # Test insert
 ll.insert(e4,3)
 # Should print 4 now
-print ll.get_position(3).value
+print(ll.get_position(3).value)
 
 # Test delete
 ll.delete(1)
 # Should print 2 now
-print ll.get_position(1).value
+print(ll.get_position(1).value)
 # Should print 4 now
-print ll.get_position(2).value
+print(ll.get_position(2).value)
 # Should print 3 now
-print ll.get_position(3).value
+print(ll.get_position(3).value)

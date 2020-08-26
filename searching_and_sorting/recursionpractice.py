7ef6779e37cd29dcacb9c46f84af820415e14efa
Combatd <combatdstar@gmail.com>
2020-08-25 20:42:10 -0700

    Define get_fib(position) psuedocode

--- searching_and_sorting/recursionpractice.py
+++ searching_and_sorting/recursionpractice.py
@@ -4,6 +4,16 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
+    # base cases based on value of position
+    # if position is equal to 1
+        
+        # return [0] which is a one element array
+    # else if position is equal to 2
+        
+        # return [0, 1] which is a two element array
+
+    # make recursive call here 
+    
     return -1
 
 # Test cases

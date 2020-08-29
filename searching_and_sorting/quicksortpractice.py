9515f1cf27cb99b462b6594e52f8322a9124856c
Combatd <combatdstar@gmail.com>
2020-08-29 04:13:47 -0700

    redefine pviot, compared_number, other variables

--- searching_and_sorting/quicksortpractice.py
+++ searching_and_sorting/quicksortpractice.py
@@ -2,24 +2,24 @@
 Input a list.
 Output a sorted list."""
 
-
+# still needs to be solved properly
 def quicksort(array):
     midpoint = int(len(array) / 2)
-    right_index = len(array)
-    left_index = 0
 
-    pivot = array[right_index - 1]
-    before_pivot = array[right_index - 2]
-    compared_number = array[left_index]
-
-    if pivot < midpoint: return array
-
-    print(midpoint, ' <- midpoint ', pivot, ' <- pivot', before_pivot, '<- before_pivot',  compared_number, ' <- compared_number')
-    if pivot_lower_than_compared_number(pivot, compared_number):
-        first_num = array.pop(left_index)
-        array.append(first_num)  # moves first number to the end
-        before_last_num = array.pop(right_index - 2) 
-        array.insert(0, before_last_num) # moves second to last number to the front
+    pivot = array[midpoint]
+    before_pivot = array[len(array) - 2]
+    
+    index = 0
+    second_index = len(array) - 1
+    compared_number = array[second_index]
+    while index < len(array) - 1:
+        if pivot_lower_than_compared_number(array[index], array[second_index]):
+            first_num = array.pop(index + 1)
+            array.append(first_num)  # moves first number to the end
+            before_last_num = array.pop(second_index - 1) 
+            array.insert(0, before_last_num) # moves second to last number to the front
+        index += 1
+        second_index -= 1
 
     return array
 
